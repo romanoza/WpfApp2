e7dbbb8b35d88b5b06287c618c6bf42b801d8bae
Roman <kowaliszyn@gazeta.pl>
2020-06-18 07:50:21 +0200

    Add project files.

---
+++ WpfApp2/ViewModel.cs
@@ -0,0 +1,55 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public class ViewModel: ViewModelBase
+    {
+        private RelayCommand _removeCommand;
+
+        public RelayCommand RemoveCommand {
+            get {
+                return _removeCommand ?? (_removeCommand = new RelayCommand(RemoveCommandAction, RemoveCommandCanExecute));
+            }
+        }
+
+        private void RemoveCommandAction() {
+
+        }
+
+        private bool RemoveCommandCanExecute() {
+            return true;
+        }
+
+        private RelayCommand _addCommand;
+
+        public RelayCommand AddCommand {
+            get {
+                return _addCommand ?? (_addCommand = new RelayCommand(AddCommandAction));
+            }
+        }
+
+        private void AddCommandAction() {
+
+        }
+
+        private RelayCommand _setIndexCommand;
+
+        public RelayCommand SetIndexCommand {
+            get {
+                return _setIndexCommand ?? (_setIndexCommand = new RelayCommand(SetIndexCommandAction));
+            }
+        }
+
+        private void SetIndexCommandAction() {
+
+        }
+
+
+    }
+}
